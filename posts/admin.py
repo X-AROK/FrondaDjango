25017a6f27c135d3d2180d381b053b879d9605c9
X-AROK <an_xarok@mail.ru>
2020-12-10 12:27:06 +0300

    Added studio, dubber, timer to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Post
 from .models import Video
+from .models import Studio
+from .models import Dubber
+from .models import Timer
 
 
 @admin.register(Post)
@@ -10,4 +13,19 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Studio)
+class StudioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dubber)
+class DubberAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Timer)
+class TimerAdmin(admin.ModelAdmin):
     pass
